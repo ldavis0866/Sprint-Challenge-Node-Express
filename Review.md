d3de26850028963641c99c862463ab90e846fa0b
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-01 17:28:25 -0500

    questions answered

--- Review.md
+++ Review.md
@@ -1,17 +1,24 @@
 # Review Questions
 
 ## What is Node.js?
+is a JS runtime built on Chromes V8 engine
 
 ## What is Express?
+a node application server framework
 
 ## Mention two parts of Express that you learned about this week.
+require and get
 
 ## What is Middleware?
+software that acts as a bridge between an operating system or database an applications
 
 ## What is a Resource?
+one or more programs that are designed to carry out operations for a specific purpose
 
 ## What can the API return to help clients know if a request was successful?
+codes
 
 ## How can we partition our application into sub-applications?
 
 ## What is CORS and why do we need it?
+a web page can embed cross origin resources
--- server.js
+++ server.js
@@ -39,6 +39,53 @@ server.get('/api/projects/:id', (req, res) => {
     });
 });
 
+server.post('/api/projects', (req, res) => {
+    const { name, description } = req.body;
+    if (!name || !description) {
+        res.status(400).json({ errorMessage: 'No name or description' });
+        return;
+    } 
+    projects
+        .insert({ name, description })
+        .then(id => {
+            res.status(201).send(id)
+        })
+        .catch(err => {
+            console.log(err);
+    })
+})
+
+server.put('/api/projects/:id', (req, res) =>{
+    const { id } = req.params;
+    const { name, description } = req.body;
+    projects
+        .update(id, { name, description })
+        .then(response => {
+            if (response == 0) {
+                res.status(400).json({errorMessage: "Did not update"});
+            } else {
+                res.status(210).json({id, name, description });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+server.delete('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projects
+        .remove(id)
+        .then(count => {
+            if (count === 0) {
+                res.status(400).json({ errorMessage: 'Did not delete'});
+            } else {
+                res.status(201).json({message: 'successfully deleted'});
+            }
+    })
+})
+
+
 
 server.get('/api/actions', (req, res) => {
     actions
@@ -66,6 +113,61 @@ server.get('/api/actions/:id', (req, res) => {
             sendUserError(500, 'The project information could not be retrieved.', res);
     });
 });
+
+server.get('/api/projects/:userId/actions', (req, res) => {
+    const { userId } = req.params;
+    projects
+    .getProjectActions(userId)
+    .then(userActions => {
+        res.json(userActions);
+    })
+})
+
+server.post('/api/actions', (req, res) => {
+    const { project_id, description } = req.body;
+    if (!project_id || !description) {
+        res.status(400).json({ errorMessage: 'No project id or descripton' });
+        return;
+    } 
+    actions
+        .insert({ project_id, description })
+        .then(id => {
+            res.status(201).send(id)
+        })
+        .catch(err => {
+            console.log(err);
+    })
+})
+
+server.put('/api/actions/:id', (req, res) =>{
+    const { id } = req.params;
+    const { project_id, description } = req.body;
+    actions
+        .update(id, { project_id, description })
+        .then(count => {
+            if (count !== 1) {
+                res.status(400).json({errorMessage: "Did not update"});
+            } else {
+                res.status(210).json({id, project_id, description });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
+server.delete('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actions
+        .remove(id)
+        .then(count => {
+            if (count === 0) {
+                res.status(400).json({ errorMessage: 'Did not delete'});
+            } else {
+                res.status(201).json({message: 'successfully deleted'});
+            }
+    })
+})
 
 
 
