ac92ee9186519559fddb10852507723013008707
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-06-01 15:44:34 -0500

    GET endpoints created for projects and actions

--- server.js
+++ server.js
@@ -1,18 +1,71 @@
 const express = require('express');
 
-const posts = require('./data/helpers/actionModel.js');
-const users = require('./data/helpers/mappers.js');
-const tags = require('./data/helpers/projectModel.js');
+const actions = require('./data/helpers/actionModel.js');
+//const users = require('./data/helpers/mappers.js');
+const projects = require('./data/helpers/projectModel.js');
 
 const port = 5555;
 const server = express();
 server.use(express.json());
-//server.use(cors({ orign: 'https://localhost:3000' }));
-server.get('/api/users');
-server.get('/api/posts');
-server.get('/api/tags');
+server.get('/api/projects');
+server.get('/api/actions');
 
 
+server.get('/api/projects', (req, res) => {
+    projects
+    .get()
+    .then(projects => {
+        res.json(projects) ;
+    })
+    .catch(error => {
+        sendUserError(500, 'The posts information could not be retrieved.', res);
+        return;
+    });
+});
+
+server.get('/api/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projects
+    .get(id)
+    .then(projects=> {
+        if (projects.length === 0) {
+            sendUserError(404, 'The project with the specified ID does not exist.', res);
+            return;
+        }
+        res.json(projects); 
+    })
+        .catch(error => {
+            sendUserError(500, 'The project information could not be retrieved.', res);
+    });
+});
+
+
+server.get('/api/actions', (req, res) => {
+    actions
+    .get()
+    .then(actions => {
+        res.json(actions) ;
+    })
+    .catch(error => {
+        sendUserError(500, 'The posts information could not be retrieved.', res);
+        return;
+    });
+});
+server.get('/api/actions/:id', (req, res) => {
+    const { id } = req.params;
+    actions
+    .get(id)
+    .then(actions=> {
+        if (actions.length === 0) {
+            sendUserError(404, 'The project with the specified ID does not exist.', res);
+            return;
+        }
+        res.json(actions); 
+    })
+        .catch(error => {
+            sendUserError(500, 'The project information could not be retrieved.', res);
+    });
+});
 
 
 
